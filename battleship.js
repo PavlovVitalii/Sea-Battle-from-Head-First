2b6855820c42c2dad5bd4ea9ff984e4587af628d
PavlovVitalii <60616724+PavlovVitalii@users.noreply.github.com>
2023-07-31 23:42:46 +0300

    add model object

--- battleship.js
+++ battleship.js
@@ -15,6 +15,35 @@ const view = {
   },
 };
 
-view.displayHit("05");
-view.displayMiss("11");
-view.displayMessage("UUUUUUUUUUU");
+const model = {
+  boardSize: 7,
+  numShips: 3,
+  shipLength: 3,
+  shipSunk: 0,
+
+  ships: [
+    { locations: ["06", "16", "26"], hits: ["", "", ""] },
+    { locations: ["24", "34", "44"], hits: ["", "", ""] },
+    { locations: ["10", "11", "12"], hits: ["", "", ""] },
+  ],
+
+  fire: function (guess) {
+    for (let i = 0; i < this.shipLength; i++) {
+      const ship = this.ships[i];
+      const index = ship.locations.indexOf(guess);
+      if (index >= 0) {
+        this.hits[index] = "hit";
+        if (condition) {
+            
+        }
+        return true;
+      }
+    }
+
+    return false;
+  },
+
+  isSunk: function (ship) {
+    return ship.hits.includes("") ? false : true;
+  },
+};
